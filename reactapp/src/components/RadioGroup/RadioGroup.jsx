c0885950ae5f86eb0110ce2bab87014c5111ac19
MrMancs <marcell.szatmari2007@gmail.com>
2025-10-01 14:18:32 +0200

    Implement state

---
+++ reactapp/src/components/RadioGroup/RadioGroup.jsx
@@ -0,0 +1,28 @@
+import { Fragment, useState } from "react";
+
+export default function RadioGroup({name, ids, values, labels, children}){
+    const [choice, setChoice] = useState("adv-radio1")
+
+    const handleRadioChange = e => {
+        console.log("handleRadioChange e", e)
+        setChoice(e.id)
+    }
+
+    return(
+        <div className="radio-group">
+            {ids.map((id, idx) => 
+            <Fragment key={idx}>
+                <input
+                    type = "radio"
+                    id = {id}
+                    name = {name}
+                    value = {values[idx]}
+                    onChange={()=>handleRadioChange({id})}
+                />
+                <label htmlFor={id}>{labels[idx]}</label>
+                {/*<p>{JSON.stringify(children)}</p>*/}
+            </Fragment>)}
+            
+        </div>
+    )
+}
