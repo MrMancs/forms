2b76ed769ca440cd7b25b1f1a98442f0b9b143d0
MrMancs <marcell.szatmari2007@gmail.com>
2025-10-01 13:35:44 +0200

    Outsource radio group

--- reactapp/src/pages/AdvancedPage.jsx
+++ reactapp/src/pages/AdvancedPage.jsx
@@ -27,23 +27,29 @@ export default class AdvancedPage extends React.Component {
         this.setState(newState)
     }
 
+    getRadioGroup = () => <div className="radio-group">
+                            <input type="radio" id="adv-radio1" name="adv-radio" value="A" /><label htmlFor="adv-radio1">A</label>
+                            <input type="radio" id="adv-radio2" name="adv-radio" value="B" /><label htmlFor="adv-radio2">B</label>
+                            <input type="radio" id="adv-radio3" name="adv-radio" value="C" /><label htmlFor="adv-radio3">C</label>
+                        </div>
+
+    radioGroupInstance = this.getRadioGroup()
+
 
     render() {
         return (
             <div id="content-advanced" className="tab-content active">
                 <h2>Advanced Form Elements</h2>
                 <form autoComplete="on">
-                    {/* TODO <InputField type="text" */}
-                    <InputField 
-                        type="text"
-                        name="adv-text"
-                        label="Text:"
-                        placeholder="Text input sample"
-                        onValueChange={this.handleAdvTextValueChange}
-                    />
                     <div className="form-row">
-                        <label htmlFor="adv-text">Text:</label>
-                        <input type="text" placeholder="Text input sample" />
+                        {/* TODO <InputField type="text" */}
+                        <InputField 
+                            type="text"
+                            name="adv-text"
+                            label="Text:"
+                            placeholder="Text input sample"
+                            onValueChange={this.handleAdvTextValueChange}
+                        />
                     </div>
                     <div className="form-row">
                         <label>Checkbox:</label>
@@ -63,11 +69,8 @@ export default class AdvancedPage extends React.Component {
                     </div>
                     <div className="form-row">
                         <label>Radio group:</label>
-                        <div className="radio-group">
-                            <input type="radio" id="adv-radio1" name="adv-radio" value="A" /><label htmlFor="adv-radio1">A</label>
-                            <input type="radio" id="adv-radio2" name="adv-radio" value="B" /><label htmlFor="adv-radio2">B</label>
-                            <input type="radio" id="adv-radio3" name="adv-radio" value="C" /><label htmlFor="adv-radio3">C</label>
-                        </div>
+                        {this.radioGroupInstance}
+                        
                     </div>
                     <div className="form-row">
                         <label htmlFor="adv-color">Color picker:</label>
